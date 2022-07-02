ff09bb18b3328e245e3ea8aee6e239da3f649dcd
md-alif10000 <md.alif10000@gmail.com>
2022-07-02 12:40:53 +0600

    ..

--- controllers/productController.js
+++ controllers/productController.js
@@ -45,27 +45,28 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Product
 exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
-  const resultPerPage = 4;
-  const productsCount = await Product.countDocuments();
+  // const resultPerPage = 4;
+  // const productsCount = await Product.countDocuments();
 
-  const apiFeature = new ApiFeatures(Product.find(), req.query)
-    .search()
-    .filter();
+  // const apiFeature = new ApiFeatures(Product.find(), req.query)
+  //   .search()
+  //   .filter();
 
-  let products = await apiFeature.query;
+  // let products = await apiFeature.query;
 
-  let filteredProductsCount = products.length;
+  // let filteredProductsCount = products.length;
 
-  apiFeature.pagination(resultPerPage);
+  // apiFeature.pagination(resultPerPage);
 
-  products = await apiFeature.query;
+  // products = await apiFeature.query;
+  const products = await Product.find();
 
   res.status(200).json({
     success: true,
     products,
-    productsCount,
-    resultPerPage,
-    filteredProductsCount,
+    // productsCount,
+    // resultPerPage,
+    // filteredProductsCount,
   });
 });
 
